63ea203882294b92e16736b043157cdf55c27919
Hessam839 <hessam.h.ir@gmail.com>
2023-02-20 20:31:38 +0330

    implement stringify interface for im

--- internalmessage.go
+++ internalmessage.go
@@ -1,12 +1,23 @@
 package proto
 
+import "fmt"
+
 type InternalMessage struct {
 	ConnectionID string `json:"connection_id"`
 	ServiceID    string `json:"service_id"`
-	Message      string `json:"message"`
 	RoutingKey   string `json:"routing_key"`
 
 	Headers    map[string]string `json:"headers"`
 	StatusCode int               `json:"status_code"`
 	Body       []byte            `json:"body"`
+}
+
+func (im *InternalMessage) String() string {
+	return fmt.Sprintf("[ConnectionId: %s, ServiceID: %s, RoutingKey: %s, Headers: %+v, StatusCode: %d, Body: %s]",
+		im.ConnectionID,
+		im.ServiceID,
+		im.RoutingKey,
+		im.Headers,
+		im.StatusCode,
+		string(im.Body))
 }
