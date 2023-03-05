36fad7c8d2afe7559890c2955d62f4a61d7007ba
Hessam839 <hessam.h.ir@gmail.com>
2023-03-05 21:11:29 +0330

    define seat formation

--- models/arrangement.go
+++ models/arrangement.go
@@ -11,4 +11,33 @@ type ArrangementModel struct {
 }
 
 type SeatFormationModel struct {
+	Zones []Zone `json:"zones"`
+}
+
+type Zone struct {
+	ZoneID    string `json:"zone_id"`
+	Available bool   `json:"available"`
+	Rows      []Row  `json:"rows"`
+}
+
+type Row struct {
+	RowNumber int    `json:"row_number"`
+	Available bool   `json:"available"`
+	Seats     []Seat `json:"seats"`
+}
+
+// Seat struct define seats metadata
+type Seat struct {
+	//SeatNumber show number of seat in row
+	SeatNumber int64 `json:"seat_number"`
+	//SeatId show seat unique id
+	SeatID string `json:"seat_id"`
+	//Status define seat status [available, reserved, booked]
+	Status string `json:"status"`
+	//PriceGroup show this seat is member of which pricing group
+	PriceGroup string `json:"price_group"`
+	//ProfileID show which user booked this seat
+	ProfileID string `json:"profile_id"`
+	//SeatType show which seat type [vip, first class, second class, etc]
+	SeatType string `json:"seat_type"`
 }
