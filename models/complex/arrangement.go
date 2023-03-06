9bef700a6382b390363d2041020f8cc3617c8fa3
Hessam839 <hessam.h.ir@gmail.com>
2023-03-06 13:55:37 +0330

    add images field to arrangement

--- models/complex/arrangement.go
+++ models/complex/arrangement.go
@@ -1,14 +1,15 @@
 package complex
 
 type ArrangementModel struct {
-	ArrangementID   string `json:"arrangement_id"`
-	Name            string `json:"name"`
-	Creator         string `json:"creator"`
-	HallID          string `json:"hall_id"`
-	IsCustom        bool   `json:"is_custom"`
-	SeatCount       int    `json:"seat_count"`
-	HaveBalcony     bool   `json:"have_balcony"`
-	SeatArrangement []byte `json:"seat_arrangement"`
+	ArrangementID   string   `json:"arrangement_id"`
+	Name            string   `json:"name"`
+	Creator         string   `json:"creator"`
+	HallID          string   `json:"hall_id"`
+	IsCustom        bool     `json:"is_custom"`
+	SeatCount       int      `json:"seat_count"`
+	HaveBalcony     bool     `json:"have_balcony"`
+	SeatArrangement []byte   `json:"seat_arrangement"`
+	Images          []string `json:"images"`
 }
 
 type SeatFormationModel struct {
