209f17198850df78e49cd1ae2a9ca29e990bd7e6
Hessam839 <hessam.h.ir@gmail.com>
2023-03-12 21:35:14 +0330

    add structs needed for profile

---
+++ models/profile/gendertype.go
@@ -0,0 +1,6 @@
+package profile
+
+type GenderType struct {
+	ID   int8   `json:"id,omitempty"`
+	Name string `json:"name"`
+}
---
+++ models/profile/profile.go
@@ -0,0 +1,20 @@
+package profile
+
+import "github.com/gofrs/uuid"
+
+type Profile struct {
+	ID            uuid.UUID `json:"id,omitempty"`
+	WalletID      uuid.UUID `json:"wallet_id"`
+	ClubID        uuid.UUID `json:"club_id,omitempty"`
+	IsClubAdmin   bool      `json:"is_club_admin,omitempty"`
+	UserID        uuid.UUID `json:"user_id"`
+	FirstName     string    `json:"first_name"`
+	LastName      string    `json:"last_name"`
+	OstanID       int       `json:"ostan_id"`
+	CityID        int       `json:"city_id"`
+	NationalID    string    `json:"national_id,omitempty"`
+	GenderID      int8      `json:"gender_id"`
+	WantReminder  bool      `json:"want_reminder,omitempty"`
+	WantPromotion bool      `json:"want_promotion,omitempty"`
+	AvatarURL     string    `json:"avatar_url,omitempty"`
+}
