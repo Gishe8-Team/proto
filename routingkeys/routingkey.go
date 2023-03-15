1572b4a05c5638b9149a0897adafe6824b3dbbe5
Hessam839 <hessam.h.ir@gmail.com>
2023-03-15 09:35:31 +0330

    adding missing routing keys

--- routingkeys/routingkey.go
+++ routingkeys/routingkey.go
@@ -1,10 +1,19 @@
 package routingkeys
 
 const (
-	RKComplex  = "complex"
-	RKGeo      = "geo"
-	RKPayment  = "payment"
-	RKEvent    = "event"
-	RKNotify   = "notify"
 	RKResponse = "response"
+	RKDms      = "dms"
+	RKTicket   = "ticket"
+	RKGeo      = "geo"
+	RKComplex  = "complex"
+	RKEvent    = "event"
+	RKSso      = "sso"
+	RKProfile  = "profile"
+	RKClub     = "club"
+	RKMeta     = "meta"
+	RKComment  = "comment"
+	RKBooking  = "booking"
+	RKPayment  = "payment"
+	RKNotify   = "notify"
+	RKMetric   = "metric"
 )
