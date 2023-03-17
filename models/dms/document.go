01d3fa71de883b799e548bd315951f99933b90cc
ParsaPM <parsamoyassari@gmail.com>
2023-03-17 04:46:17 +0330

    last event changes

--- models/dms/document.go
+++ models/dms/document.go
@@ -1,7 +1,9 @@
 package dms
 
+import "github.com/volatiletech/null/v8"
+
 type QueryDocumentsModels struct {
-	ID      string `boil:"id" json:"id"`
-	OwnerID string `boil:"owner_id" json:"owner_id"`
-	DMSType string `boil:"dms_type" json:"type"`
+	ID      null.String `boil:"id" json:"id"`
+	OwnerID null.String `boil:"owner_id" json:"owner_id"`
+	DMSType null.String `boil:"dms_type" json:"type"`
 }
