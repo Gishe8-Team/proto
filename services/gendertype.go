57549949e4a5eb8f1a76c68979e12d7897f6ef35
Hessam839 <hessam.h.ir@gmail.com>
2023-03-17 17:41:24 +0330

    extending gender type service id

--- services/gendertype.go
+++ services/gendertype.go
@@ -1,8 +1,9 @@
 package services
 
 const (
-	CreateGenderType = "3.2.1"
-	EditGenderType   = "3.2.2"
-	DeleteGenderType = "3.2.3"
-	ListGenderTypes  = "3.2.4"
+	CreateGenderType   = "3.2.1"
+	EditGenderType     = "3.2.2"
+	DeleteGenderType   = "3.2.3"
+	ListGenderTypes    = "3.2.4"
+	FindGenderTypeByID = "3.2.5"
 )
