bd14a8940213246e2cac7b3dc97c2095436e383b
Hessam839 <hessam.h.ir@gmail.com>
2023-03-17 18:47:25 +0330

    adding boil tag to profile

--- models/profile/profile.go
+++ models/profile/profile.go
@@ -3,21 +3,21 @@ package profile
 import "github.com/gofrs/uuid"
 
 type Profile struct {
-	ID            uuid.UUID `json:"id,omitempty"`
-	WalletID      uuid.UUID `json:"wallet_id"`
-	ClubID        uuid.UUID `json:"club_id,omitempty"`
-	IsClubAdmin   bool      `json:"is_club_admin,omitempty"`
-	UserID        uuid.UUID `json:"user_id"`
-	FirstName     string    `json:"first_name"`
-	LastName      string    `json:"last_name"`
-	OstanID       int       `json:"ostan_id"`
-	CityID        int       `json:"city_id"`
-	NationalID    string    `json:"national_id,omitempty"`
-	GenderID      int16     `json:"gender_id"`
-	GenderName    string    `json:"gender_name,omitempty"`
-	WantReminder  bool      `json:"want_reminder,omitempty"`
-	WantPromotion bool      `json:"want_promotion,omitempty"`
-	AvatarURL     string    `json:"avatar_url,omitempty"`
+	ID            uuid.UUID `boil:"id" json:"id,omitempty"`
+	WalletID      uuid.UUID `boil:"wallet_id" json:"wallet_id"`
+	ClubID        uuid.UUID `boil:"club_id" json:"club_id,omitempty"`
+	IsClubAdmin   bool      `boil:"is_club_admin" json:"is_club_admin,omitempty"`
+	UserID        uuid.UUID `boil:"user_id" json:"user_id"`
+	FirstName     string    `boil:"first_name" json:"first_name"`
+	LastName      string    `boil:"last_name" json:"last_name"`
+	OstanID       int       `boil:"ostan_id" json:"ostan_id"`
+	CityID        int       `boil:"city_id" json:"city_id"`
+	NationalID    string    `boil:"national_id" json:"national_id,omitempty"`
+	GenderID      int16     `boil:"gender_id" json:"gender_id"`
+	GenderName    string    `boil:"gender_name" json:"gender_name,omitempty"`
+	WantReminder  bool      `boil:"want_reminder" json:"want_reminder,omitempty"`
+	WantPromotion bool      `boil:"want_promotion" json:"want_promotion,omitempty"`
+	AvatarURL     string    `boil:"avatar_url" json:"avatar_url,omitempty"`
 }
 
 type QueryProfile struct {
