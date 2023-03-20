c1afbfbb41b55b8ac1ec477a9f1bb2d7b911e5d5
Hessam839 <hessam.h.ir@gmail.com>
2023-03-20 22:08:56 +0330

    adding sms and email struct

--- models/notify/notify.go
+++ models/notify/notify.go
@@ -1,11 +1,12 @@
 package notify
 
 type SmsModel struct {
-	MessagesText  string
-	MobileNumbers []string
+	MessagesText  string   `json:"messages_text"`
+	MobileNumbers []string `json:"mobile_numbers"`
 }
 
 type EmailModel struct {
-	Message        string
-	EmailAddresses []string
+	Message        string   `json:"message"`
+	Subject        string   `json:"subject"`
+	EmailAddresses []string `json:"email-addresses"`
 }
