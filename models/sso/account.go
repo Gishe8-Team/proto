c457db67f377e9d3fd09ba6f4b8511021d8c73be
Hessam839 <hessam.h.ir@gmail.com>
2023-03-30 17:55:06 +0330

    change sso.account struct

--- models/sso/account.go
+++ models/sso/account.go
@@ -3,15 +3,18 @@ package sso
 import "github.com/volatiletech/null/v8"
 
 type Account struct {
-	ID             string      `boil:"id" json:"id" toml:"id" yaml:"id"`
-	UserName       null.String `boil:"user_name" json:"user_name,omitempty" toml:"user_name" yaml:"user_name,omitempty"`
-	Password       null.String `boil:"password" json:"password,omitempty" toml:"password" yaml:"password,omitempty"`
-	GroupID        int16       `boil:"group_id" json:"group_id" toml:"group_id" yaml:"group_id"`
-	UserACL        null.String `boil:"user_acl" json:"user_acl,omitempty" toml:"user_acl" yaml:"user_acl,omitempty"`
-	UserEmail      null.String `boil:"user_email" json:"user_email,omitempty" toml:"user_email" yaml:"user_email,omitempty"`
-	EmailVerified  null.Bool   `boil:"email_verified" json:"email_verified,omitempty" toml:"email_verified" yaml:"email_verified,omitempty"`
-	Mobile         null.String `boil:"mobile" json:"mobile,omitempty" toml:"mobile" yaml:"mobile,omitempty"`
-	MobileVerified null.Bool   `boil:"mobile_verified" json:"mobile_verified,omitempty" toml:"mobile_verified" yaml:"mobile_verified,omitempty"`
+	ID               string      `boil:"id" json:"id" toml:"id" yaml:"id"`
+	UserName         null.String `boil:"user_name" json:"user_name,omitempty" toml:"user_name" yaml:"user_name,omitempty"`
+	Password         null.String `boil:"password" json:"password,omitempty" toml:"password" yaml:"password,omitempty"`
+	GroupID          int         `boil:"group_id" json:"group_id" toml:"group_id" yaml:"group_id"`
+	UserACL          string      `boil:"user_acl" json:"user_acl" toml:"user_acl" yaml:"user_acl"`
+	UserEmail        null.String `boil:"user_email" json:"user_email,omitempty" toml:"user_email" yaml:"user_email,omitempty"`
+	IsEmailVerified  null.Bool   `boil:"is_email_verified" json:"is_email_verified,omitempty" toml:"is_email_verified" yaml:"is_email_verified,omitempty"`
+	UserMobile       null.String `boil:"user_mobile" json:"user_mobile,omitempty" toml:"user_mobile" yaml:"user_mobile,omitempty"`
+	IsMobileVerified null.Bool   `boil:"is_mobile_verified" json:"is_mobile_verified,omitempty" toml:"is_mobile_verified" yaml:"is_mobile_verified,omitempty"`
+	CreatedAt        null.Time   `boil:"created_at" json:"created_at,omitempty" toml:"created_at" yaml:"created_at,omitempty"`
+	UpdatedAt        null.Time   `boil:"updated_at" json:"updated_at,omitempty" toml:"updated_at" yaml:"updated_at,omitempty"`
+	DeletedAt        null.Time   `boil:"deleted_at" json:"deleted_at,omitempty" toml:"deleted_at" yaml:"deleted_at,omitempty"`
 }
 
 type AccountSlice []Account
