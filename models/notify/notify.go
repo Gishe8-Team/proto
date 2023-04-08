5310ebdb36a07fc756748fe5fc57abc4d3f3ffe1
Hessam839 <hessam.h.ir@gmail.com>
2023-04-08 22:01:55 +0330

    add smsarchive and emailarchive struct

--- models/notify/notify.go
+++ models/notify/notify.go
@@ -3,10 +3,26 @@ package notify
 type SmsModel struct {
 	MessagesText  string   `json:"messages_text"`
 	MobileNumbers []string `json:"mobile_numbers"`
+	Service       string   `json:"service,omitempty"`
 }
 
 type EmailModel struct {
 	Message        string   `json:"message"`
 	Subject        string   `json:"subject"`
 	EmailAddresses []string `json:"email-addresses"`
+	Service        string   `json:"service,omitempty"`
+}
+
+type SMSArchiveSlice struct {
+	ID        string `json:"id"`
+	MobileNum string `json:"mobile_num"`
+	Message   string `json:"message"`
+	Service   string `json:"service"`
+}
+
+type EmailArchiveSlice struct {
+	ID           string `json:"id"`
+	EmailAddress string `json:"email_address"`
+	Message      string `json:"message"`
+	Service      string `json:"service"`
 }
