f2d4755f4151079baa2bb21b83086b45a61c350b
Hessam839 <hessam.h.ir@gmail.com>
2023-04-08 22:40:40 +0330

    define slice type of smsarchive and emailarchive

--- models/notify/notify.go
+++ models/notify/notify.go
@@ -14,31 +14,32 @@ type EmailModel struct {
 }
 
 type SMSArchiveRequest struct {
-	UserID       string `json:"user_id,omitempty"`
 	MobileNumber string `json:"mobile_number,omitempty"`
 	Service      string `json:"service,omitempty"`
 	Limit        int    `json:"limit,omitempty"`
 	Offset       int    `json:"offset,omitempty"`
 }
 
-type SMSArchiveSlice struct {
-	ID           string `json:"id" boil:"id"`
+type SMSArchive struct {
 	MobileNum    string `json:"mobile_num" boil:"mobile_num"`
 	Message      string `json:"message" boil:"message"`
 	WhichService string `json:"which_service" boil:"which_service"`
 }
 
+type SMSArchiveSlice []*SMSArchive
+
 type EmailArchiveRequest struct {
-	UserID       string `json:"user_id,omitempty"`
 	EmailAddress string `json:"email_address,omitempty"`
 	Service      string `json:"service,omitempty"`
 	Limit        int    `json:"limit,omitempty"`
 	Offset       int    `json:"offset,omitempty"`
 }
 
-type EmailArchiveSlice struct {
+type EmailArchive struct {
 	ID           string `json:"id" boil:"id"`
 	EmailAddress string `json:"email_address" boil:"email_address"`
 	EmailMessage string `json:"email_message" boil:"email_message"`
 	WhichService string `json:"which_service" boil:"which_service"`
 }
+
+type EmailArchiveSlice []*EmailArchive
