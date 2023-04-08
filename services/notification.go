0df33c6ad73c1b5ccbea50450e53ee2cc750f587
Hessam839 <hessam.h.ir@gmail.com>
2023-04-08 08:18:57 +0330

    add SendCodeSMS and SendCodeEmail services to notify

--- services/notification.go
+++ services/notification.go
@@ -4,11 +4,13 @@ const (
 	SendAnSMS         = "15.1.1"
 	SendBulkSMS       = "15.1.2"
 	GetAccountBalance = "15.1.3"
+	SendCodeSMS       = "15.1.4"
 )
 
 const (
 	SendAnEmail   = "15.2.1"
 	SendBulkEmail = "15.2.2"
+	SendCodeEmail = "15.2.3"
 )
 
 const (
