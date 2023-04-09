a6ec8a9bb2887720de71823cf53e3b09efd8184d
Hessam839 <hessam.h.ir@gmail.com>
2023-04-09 20:02:11 +0330

    add redirect to struct

--- models/notify/sms.go
+++ models/notify/sms.go
@@ -11,3 +11,7 @@ type BulkSmsModel struct {
 	MobileNumbers []string `json:"mobile_numbers"`
 	Service       string   `json:"service,omitempty"`
 }
+
+type Redirect struct {
+	RedirectTo string `json:"redirect_to"`
+}
