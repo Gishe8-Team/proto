a00e1409f0af3b8af137708da47e957180dc6757
Hessam839 <hessam.h.ir@gmail.com>
2023-04-10 20:44:24 +0330

    add CodeEmailModel

--- models/notify/email.go
+++ models/notify/email.go
@@ -1,6 +1,18 @@
 package notify
 
 type EmailModel struct {
+	Message        string `json:"message"`
+	Subject        string `json:"subject"`
+	EmailAddresses string `json:"email-addresses"`
+	Service        string `json:"service,omitempty"`
+}
+
+type CodeEmailModel struct {
+	EmailModel
+	CodeToken string `json:"code_token"`
+}
+
+type BulkEmailModel struct {
 	Message        string   `json:"message"`
 	Subject        string   `json:"subject"`
 	EmailAddresses []string `json:"email-addresses"`
