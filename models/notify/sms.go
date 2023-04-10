67013cf32d672f5558ecb50cf73bb8f0b1824793
Hessam839 <hessam.h.ir@gmail.com>
2023-04-10 20:44:17 +0330

    add CodeSmsModel

--- models/notify/sms.go
+++ models/notify/sms.go
@@ -6,6 +6,11 @@ type SmsModel struct {
 	Service       string `json:"service,omitempty"`
 }
 
+type CodeSmsModel struct {
+	SmsModel
+	CodeToken string `json:"code_token"`
+}
+
 type BulkSmsModel struct {
 	MessagesText  string   `json:"messages_text"`
 	MobileNumbers []string `json:"mobile_numbers"`
@@ -14,4 +19,5 @@ type BulkSmsModel struct {
 
 type Redirect struct {
 	RedirectTo string `json:"redirect_to"`
+	CodeToken  string `json:"code_token"`
 }
