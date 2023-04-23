431ad41d0eb9dd883b2cebcfe520b28214cdd78b
Hessam839 <hessam.h.ir@gmail.com>
2023-04-23 19:20:41 +0330

    adding token structs

---
+++ models/sso/token.go
@@ -0,0 +1,23 @@
+package sso
+
+import "time"
+
+type Claims struct {
+	Issuer         string    `json:"iss"`
+	Subject        string    `json:"sub"`
+	Audience       []string  `json:"aud"`
+	ExpirationTime time.Time `json:"exp"`
+	NotBefore      time.Time `json:"nbf"`
+	IssuedAt       time.Time `json:"iat"`
+}
+type LoginToken struct {
+	Claims
+	UserID string   `json:"user_id"`
+	ACL    []string `json:"acl"`
+}
+
+type CodeToken struct {
+	Claims
+	Mobile string `json:"mobile,omitempty"`
+	Email  string `json:"email,omitempty"`
+}
