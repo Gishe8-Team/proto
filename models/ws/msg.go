815dcef0f84794a34111288cc1a6ceaa2f83c817
Hessam839 <hessam.h.ir@gmail.com>
2023-04-30 23:56:07 +0330

    add comments

--- models/ws/msg.go
+++ models/ws/msg.go
@@ -1,14 +1,16 @@
 package ws
 
+// BookSeatMsg request message for booking a seat for particular timeslot.
 type BookSeatMsg struct {
-	TimeslotID string `json:"timeslot_id"`
-	Hash       string `json:"hash"`
-	SeatID     string `json:"seat_id"`
+	TimeslotID string `json:"timeslot_id"` // ID of the timeslot being booked
+	Hash       string `json:"hash"`        // Used to verify authenticity of message
+	SeatID     string `json:"seat_id"`     // ID of the seat being booked
 }
 
+// BookingSeatEvent represents an event related to booking a seat for a particular timeslot.
 type BookingSeatEvent struct {
-	TimeslotID string `json:"timeslot_id"`
-	Hash       string `json:"hash"`
-	SeatID     string `json:"seat_id"`
-	Status     string `json:"status"`
+	TimeslotID string `json:"timeslot_id"` // The ID of the timeslot for which the seat was booked.
+	Hash       string `json:"hash"`        // An authentication or authorization token associated with the booking.
+	SeatID     string `json:"seat_id"`     // The ID of the booked seat.
+	Status     string `json:"status"`      // The current status of the booking (e.g. "confirmed", "pending", "cancelled", etc.).
 }
