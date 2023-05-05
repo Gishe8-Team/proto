68503432eed9bb4bb80fed34c378cdd974fb50fc
Hessam839 <hessam.h.ir@gmail.com>
2023-05-05 19:25:11 +0330

    add some booking service

--- services/booking.go
+++ services/booking.go
@@ -1,5 +1,12 @@
 package services
 
 const (
-	CreateBookingSlot = "8.1.1"
+	CreateBookingSlot   = "8.1.1"
+	FinalizeBookingSlot = "8.1.2"
+	GetBookingSlot      = "8.1.3"
+)
+
+const (
+	CartFindByHash = "8.2.1"
+	CartFinalizing = "8.2.2"
 )
