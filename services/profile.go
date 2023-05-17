8cd7e824032b1c92d77b888029862fe86c2d89bb
ParsaPM <parsamoyassari@gmail.com>
2023-05-17 22:02:05 +0330

    remove FindProfileByProfileID from profile service

--- services/profile.go
+++ services/profile.go
@@ -6,9 +6,8 @@ const (
 	DeleteProfile           = "3.1.3"
 	JoinProfileToClub       = "3.1.4"
 	ListProfiles            = "3.1.5"
-	FindProfileByProfileID  = "3.1.6"
-	FindProfileByUserID     = "3.1.7"
-	FindProfileByNationalID = "3.1.8"
-	FindProfileByClubID     = "3.1.9"
-	AddAvatar               = "3.1.10"
+	FindProfileByUserID     = "3.1.6"
+	FindProfileByNationalID = "3.1.7"
+	FindProfileByClubID     = "3.1.8"
+	AddAvatar               = "3.1.9"
 )
