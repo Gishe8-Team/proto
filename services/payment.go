dd7513c62f57815d0796b8184f9a8c8c8d552dca
Hessam839 <hessam.h.ir@gmail.com>
2023-06-10 18:33:03 +0330

    add payment services

--- services/payment.go
+++ services/payment.go
@@ -4,6 +4,7 @@ const (
 	CreateWallet           = "10.1.1"
 	EditWallet             = "10.1.2"
 	ListWalletTransactions = "10.1.3"
+	FindWalletByID         = "10.1.4"
 )
 
 const (
@@ -13,4 +14,17 @@ const (
 	ListActiveBankGateway = "10.2.4"
 	FindBankGateway       = "10.2.5"
 	ProcessBankCallBack   = "10.2.6"
+)
+
+const (
+	CreateTransaction = "10.3.1"
+	FindTransaction   = "10.3.2"
+	EditTransaction   = "10.3.3"
+)
+
+const (
+	CreateBankAccount = "10.4.1"
+	EditBankAccount   = "10.4.2"
+	DeleteBankAccount = "10.4.3"
+	FindAccount       = "10.4.4"
 )
