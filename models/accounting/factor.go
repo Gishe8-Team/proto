0134ef31653e294a4aca04d4508fdc38c2829b01
Hessam839 <hessam.h.ir@gmail.com>
2023-06-19 21:44:39 +0330

    change some accounting structs

--- models/accounting/factor.go
+++ models/accounting/factor.go
@@ -35,24 +35,27 @@ const (
 //	Data map[string]interface{} `json:"data"`
 //}
 
+// Faktor is an object representing the database table.
 type Faktor struct {
 	ID            string      `boil:"id" json:"id" toml:"id" yaml:"id"`
 	UserID        string      `boil:"user_id" json:"user_id" toml:"user_id" yaml:"user_id"`
-	CartID        string      `boil:"cart_id" json:"cart_id" toml:"cart_id" yaml:"cart_id"`
+	CartID        null.String `boil:"cart_id" json:"cart_id,omitempty" toml:"cart_id" yaml:"cart_id,omitempty"`
 	StatusID      int16       `boil:"status_id" json:"status_id" toml:"status_id" yaml:"status_id"`
 	TypeID        int16       `boil:"type_id" json:"type_id" toml:"type_id" yaml:"type_id"`
-	TotalPrice    float64     `boil:"total_price" json:"total_price,omitempty" toml:"total_price" yaml:"total_price"`
-	Discounts     null.JSON   `boil:"discounts" json:"discounts,omitempty" toml:"discounts" yaml:"discounts"`
-	TotalDiscount float64     `boil:"total_discount" json:"total_discount,omitempty" toml:"total_discount" yaml:"total_discount"`
-	Vat           float64     `boil:"vat" json:"vat,omitempty" toml:"vat" yaml:"vat"`
-	PayableAmount float64     `boil:"payable_amount" json:"payable_amount,omitempty" toml:"payable_amount" yaml:"payable_amount"`
+	TotalPrice    float64     `boil:"total_price" json:"total_price" toml:"total_price" yaml:"total_price"`
+	TotalDiscount float64     `boil:"total_discount" json:"total_discount" toml:"total_discount" yaml:"total_discount"`
+	Vat           float64     `boil:"vat" json:"vat" toml:"vat" yaml:"vat"`
+	PayableAmount float64     `boil:"payable_amount" json:"payable_amount" toml:"payable_amount" yaml:"payable_amount"`
 	TransactionID null.String `boil:"transaction_id" json:"transaction_id,omitempty" toml:"transaction_id" yaml:"transaction_id,omitempty"`
 	TimeslotID    string      `boil:"timeslot_id" json:"timeslot_id" toml:"timeslot_id" yaml:"timeslot_id"`
-	TimeslotTitle null.String `boil:"timeslot_title" json:"timeslot_title,omitempty" toml:"timeslot_title" yaml:"timeslot_title,omitempty"`
 	Data          null.JSON   `boil:"data" json:"data,omitempty" toml:"data" yaml:"data,omitempty"`
 	CreatedAt     null.Time   `boil:"created_at" json:"created_at,omitempty" toml:"created_at" yaml:"created_at,omitempty"`
 	UpdatedAt     null.Time   `boil:"updated_at" json:"updated_at,omitempty" toml:"updated_at" yaml:"updated_at,omitempty"`
 	DeletedAt     null.Time   `boil:"deleted_at" json:"deleted_at,omitempty" toml:"deleted_at" yaml:"deleted_at,omitempty"`
+	Discounts     null.JSON   `boil:"discounts" json:"discounts,omitempty" toml:"discounts" yaml:"discounts,omitempty"`
+	TimeslotTitle null.String `boil:"timeslot_title" json:"timeslot_title,omitempty" toml:"timeslot_title" yaml:"timeslot_title,omitempty"`
+	EventID       null.String `boil:"event_id" json:"event_id,omitempty" toml:"event_id" yaml:"event_id,omitempty"`
+	EventTitle    null.String `boil:"event_title" json:"event_title,omitempty" toml:"event_title" yaml:"event_title,omitempty"`
 }
 
 type FaktorFullModel struct {
@@ -69,6 +72,8 @@ type FaktorFullModel struct {
 	TransactionID null.String `boil:"transaction_id" json:"transaction_id,omitempty" toml:"transaction_id" yaml:"transaction_id,omitempty"`
 	TimeslotID    string      `boil:"timeslot_id" json:"timeslot_id" toml:"timeslot_id" yaml:"timeslot_id"`
 	TimeslotTitle null.String `boil:"timeslot_title" json:"timeslot_title,omitempty" toml:"timeslot_title" yaml:"timeslot_title,omitempty"`
+	EventID       null.String `boil:"event_id" json:"event_id,omitempty" toml:"event_id" yaml:"event_id,omitempty"`
+	EventTitle    null.String `boil:"event_title" json:"event_title,omitempty" toml:"event_title" yaml:"event_title,omitempty"`
 	Data          null.JSON   `boil:"data" json:"data,omitempty" toml:"data" yaml:"data,omitempty"`
 	CreatedAt     null.Time   `boil:"created_at" json:"created_at,omitempty" toml:"created_at" yaml:"created_at,omitempty"`
 }
